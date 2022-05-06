f9f13daf63fbd1e9bbda466834c404ca00eb9944
Heric Virgilio <heric.v.v.santos@gmail.com>
2022-05-06 01:12:12 -0300

    commit 1 do dia 06/05/2022

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`A velociade do seu carro é  ${vel} KM/H`)
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
